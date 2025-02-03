1a678dac718562742686ba5757c8cc0894fa5281
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-03 17:37:56 +0200

    Feat(verifyOtp & login ): add login & verifyOtp endpoint

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,7 +1,9 @@
-const { signUp } = require('../Controller/authController');
+const { signUp, login, verifyOTP } = require('../Controller/authController');
 const express = require('express');
 const router = express.Router();
 
 router.post('/signup', signUp);
+router.post('/login', login);
+router.post('/verifyOTP', verifyOTP);
 
 module.exports = router
