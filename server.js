f5b24216096133b5e179f0b67c1c36004389e68f
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-03 15:14:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const appError = require('./utils/appError')
+const globalError = require('./middleware/errorMiddleware')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 
+
+app.all("*", (req, res, next) => {
+    next(new appError(`Can't find this route ${req.originalUrl}`, 400));
+  });
+  
+  // Global Error Handling Middleware For Express
+  app.use(globalError);
+
 app.listen(PORT, () => {
-    
     console.log(`server is running on port ${PORT}`)
 })
 
