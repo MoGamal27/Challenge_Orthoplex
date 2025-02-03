235fce8b4967731782eff32d6db9393870669804
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-03 18:41:06 +0200

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,10 @@
 const authRoute = require('./authRoute');
+const userRoute = require('./userRoute');
 const express = require('express');
 const router = express.Router();
 
 router.use('/auth', authRoute);
+router.use('/users', userRoute);
+
 
 module.exports = router;
