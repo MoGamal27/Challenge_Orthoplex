a335cba23f414efc96ae15623615d2b2ecee3c50
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-03 16:51:49 +0200

    create function generate otp

---
+++ utils/generateOTP.js
@@ -0,0 +1,9 @@
+const generateOTP = () => {
+    // Generate 6 digit OTP
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    return otp
+}
+    
+
+module.exports = generateOTP
