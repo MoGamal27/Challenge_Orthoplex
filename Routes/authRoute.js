9d082e97374a25104171f9911d0e8b6323a4affc
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-03 20:22:35 +0200

    add validator

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,9 +1,10 @@
 const { signUp, login, verifyOTP } = require('../Controller/authController');
+const { signupValidator, loginValidator } = require('../utils/validator/authValidator');
 const express = require('express');
 const router = express.Router();
 
-router.post('/signup', signUp);
-router.post('/login', login);
+router.post('/signup', signupValidator, signUp);
+router.post('/login', loginValidator ,login);
 router.post('/verifyOTP', verifyOTP);
 
 module.exports = router
